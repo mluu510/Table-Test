5756505525d18e3728f977a4ee78f3d28e7dc3c8
Minh Luu <mluu510@gmail.com>
2014-06-04 13:13:49 -0700

    Initial Commit

---
+++ Test Table/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Test Table
+//
+//  Created by Minh Luu on 6/4/14.
+//  Copyright (c) 2014 Minh Luu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ Test TableTests/Test_TableTests.m
@@ -0,0 +1,34 @@
+//
+//  Test_TableTests.m
+//  Test TableTests
+//
+//  Created by Minh Luu on 6/4/14.
+//  Copyright (c) 2014 Minh Luu. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Test_TableTests : XCTestCase
+
+@end
+
+@implementation Test_TableTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
